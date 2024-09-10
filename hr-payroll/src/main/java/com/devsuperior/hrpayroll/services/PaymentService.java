6e39441a77fef321558397c5c1839a328e1ec03b
Josaf4 <josafa.soares2909@gmail.com>
2024-09-10 01:37:19 -0400

    Project hr-eureka-server created

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hrpayroll.services;
+	package com.devsuperior.hrpayroll.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
