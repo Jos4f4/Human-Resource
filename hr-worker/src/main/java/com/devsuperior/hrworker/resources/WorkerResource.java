81b6c999923a183f2d0e11fe6e95bb4ffed5dab6
Josaf4 <josafa.soares2909@gmail.com>
2024-09-12 02:10:44 -0400

    Zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -36,11 +36,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		//Simulação de exceção
-		/*try {
+		try {
 			Thread.sleep(3000L);
 		}catch(InterruptedException e) {
 			e.printStackTrace();
-		}*/
+		}
 		
 		//Balaceamento de carga========
 		logger.info("PORT = " + env.getProperty("local.servr.port"));
